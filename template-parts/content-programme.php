6b9c673242ec3f06ac7981db946afc27d83517d3
Jeremie <macosphere@Mac-mini-de-Macosphere.local>
2017-10-16 18:16:48 +0200

    Page programme

--- template-parts/content-programme.php
+++ template-parts/content-programme.php
@@ -17,29 +17,13 @@
 		else :
 			the_title( '<h2 class="entry-title title typo_alpha"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
-
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php hmsphr_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
+ 		?>
 	</header><!-- .entry-header -->
+
 
 	<div class="entry-content">
 		<?php
-			the_content( sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'hmsphr' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				get_the_title()
-			) );
+			the_content();
 
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'hmsphr' ),
@@ -47,6 +31,50 @@
 			) );
 		?>
 	</div><!-- .entry-content -->
+
+
+	<h2 class="entry-title title typo_alpha">événements liés au programme</h2>
+
+	<!-- //////////////////// CONFIG  ////////////////// -->
+	<!-- //////// accueil , recherche , protolab //////// -->
+	<script>
+			var category_Name = "accueil"; // Pour les appels ajax load more; -> JS -> WP_Query
+	</script>
+	<?php $category_Name = "accueil" ?>
+
+	<?php
+	// GET ALL KEYWORDS
+	$keywords = get_terms( 'post_keyword', array(
+	    'hide_empty' => false,
+	));
+	foreach ($keywords as $keyword) {
+		echo $keyword->name;
+	}
+	// GET SLUG OF PAGE
+
+	?>
+
+	<!-- //////////////////// LOOP  ////////////////// -->
+		 <div class="postsTable">
+			 <div class="grid-sizer"></div>
+			 <div class="gutter-sizer"></div>
+
+			<?php
+			 $args = array(
+				'post_type' => array('actu', 'annonce'),
+				'posts_per_page'=> -1,
+				'category_name'=> $category_Name
+			 );
+			 $loop = new WP_Query( $args );
+			 while ( $loop->have_posts() ) : $loop->the_post();
+
+				 get_template_part( 'single-actu-and-annonce');
+
+			 endwhile; ?>
+		 </div>
+
+
+
 
 	<footer class="entry-footer">
 		<?php hmsphr_entry_footer(); ?>
