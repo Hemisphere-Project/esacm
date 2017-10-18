9c759f56a54b459985874f10a3493679edae0243
Jeremie <macosphere@Mac-mini-de-Macosphere.local>
2017-10-18 12:43:41 +0200

    Array

--- template-diplomes.php
+++ template-diplomes.php
@@ -35,47 +35,39 @@ $annees = get_terms( array(
     'taxonomy' => 'annee',
     'hide_empty' => false,
 ) );
-// MULTIDIM ARRAY [YEAR][CATEGORIE]->CHERCHEUR
 
-$membres_chercheurs_annees = array();
+// NEW ARRAY DIPLOMES EACH YEAR
+$diplomes_annees = array();
 foreach ($annees as $annee) {
 	foreach ($diplomes as $diplome) {
 		$annees_du_diplome = wp_get_post_terms($diplome->ID, 'annee', array("fields" => "all"));
-
-	}
-}
-
-$membres_chercheurs_annees = array();
-foreach ($annees as $annee) {
-	foreach ($membres_chercheurs as $chercheur) {
-		$annees_du_chercheur = wp_get_post_terms($chercheur->ID, 'annee', array("fields" => "all"));
-		foreach($annees_du_chercheur as $annee_du_chercheur){
-			if($annee_du_chercheur->name==$annee->name){
-				$categorieNum = get_post_meta($chercheur->ID, 'wpcf-categorie-chercheur', true);
-				$membres_chercheurs_annees[$annee->name][$categorieNum][]=$chercheur;
+		foreach ($annees_du_diplome as $annee_du_diplome) {
+			if($annee_du_diplome->name==$annee->name){
+				$diplomes_annees[$annee->name][]=$diplome;
 			}
 		}
 	}
 }
-
 // SORT
-krsort($membres_chercheurs_annees); //trie par la clé année en reverse order
-foreach ($membres_chercheurs_annees as $key => $value) {
-	ksort($membres_chercheurs_annees[$key]); //trie par la clé categorie
-}
-// DEBUG ARRAY
-// foreach ($membres_chercheurs_annees as $annee => $chercheurs_annees) {
-// 	echo $annee.'<br>';
-// 	foreach ($chercheurs_annees as $category => $chercheur_categories) {
-// 		echo $category.'<br>';
-// 		foreach ($chercheur_categories as $chercheur) {
-// 			// echo $chercheur->ID.'<br>';
-// 			// echo $chercheur->post_title.'<br>';
-// 			// echo get_post_meta($chercheur->ID, 'wpcf-fonction-chercheur', true).'<br>';
+krsort($diplomes_annees); //trie par la clé année en reverse order
+
+// LOOP
+// foreach ($diplomes_annees as $annee => $diplomes_annee) {
+// 	echo $annee;
+// 	// EXPOS
+// 	foreach ($diplomes_expos as $diplomes_expo) {
+// 		$annees_de_lexpo = wp_get_post_terms($diplomes_expo->ID, 'annee', array("fields" => "all"));
+// 		foreach ($annees_de_lexpo as $annee_de_lexpo) {
+// 			if($annee_de_lexpo->name==$annee){
+// 				echo $diplomes_expo->post_title;
+// 			}
 // 		}
 // 	}
+// 	// DIPLOME
+// 	foreach ($diplomes_annee as $diplome) {
+// 		echo $diplome->post_title;
+// 	}
 // }
-
 
 get_header(); ?>
 
@@ -104,55 +96,44 @@ get_header(); ?>
 			endwhile; // End of the loop.
 			?>
 
-
 			<div id="chercheurs_accordeon" class="accordeon">
+
 				<?php
-				foreach ($membres_chercheurs_annees as $annee => $chercheurs_annees) {?>
+				foreach ($diplomes_annees as $annee => $diplomes_annee) {
+					?>
 					<div class="spacer shadowedBox"></div>
 					<div class="anneeTitle typo_beta shadowed">
-
 						<div class="arrow arrowLeft">→</div>
 						<?php echo $annee;?>
 						<div class="arrow arrowRight">←</div>
 					</div>
-					<div class="anneeContent">
-					<div class="anneeContentPadding"></div>
-					<?php
-					foreach ($chercheurs_annees as $category => $chercheur_categories) { ?>
-						<div class="categoryContent">
-							<div class="categoryTitle typo_gamma">
-								<?php
-								if($category==1){echo 'RÉSIDENTS CHERCHEURS';}
-								elseif($category==2) {echo 'ÉTUDIANTS CHERCHEURS';}
-								elseif($category==3) {echo 'CHERCHEURS ASSOCIÉS';}
-								?>
-							</div>
-							<ul id="" class="membres_equipe chercheursList">
-							<?php
-							foreach ($chercheur_categories as $chercheur) {?>
-								<li class="membre">
-									<?php if($chercheur->post_content != ''){ ?>
-										<a id="<?php echo $chercheur->ID; ?>" class="link_professeur nom_complet open_in_popup" href="<?php echo get_permalink($chercheur->ID); ?>"><?php echo $chercheur->post_title; ?></a><br>
-									<?php } else{ ?>
-										<span class="nom_complet"><?php echo $chercheur->post_title; ?></span><br>
-									<?php } ?>
-									<?php $fonction = get_post_meta($chercheur->ID, 'wpcf-fonction-chercheur', true);
-									if($fonction != ''){?>
-										<span class="fonction"><?php echo $fonction; ?></span>
-									<?php } ?>
 
-								</li>
-							<?php
-						} ?>
-						</ul>
-					</div>
+
 					<?php
-				}?>
-			</div>
-			<?php
+					// EXPOS
+					foreach ($diplomes_expos as $diplomes_expo) {
+						$annees_de_lexpo = wp_get_post_terms($diplomes_expo->ID, 'annee', array("fields" => "all"));
+						foreach ($annees_de_lexpo as $annee_de_lexpo) {
+							if($annee_de_lexpo->name==$annee){
+								echo $diplomes_expo->post_title;
+							}
+						}
+					}
+					// DIPLOME
+					foreach ($diplomes_annee as $diplome) {
+						echo $diplome->post_title;
+					}
 				}
 				?>
-		</div>
+
+			</div>
+
+
+
+
+
+
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
