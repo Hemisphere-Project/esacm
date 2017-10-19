7f65e1b0e31744be8fc80990ef02a2042caf58c3
Tamias <imbert.matthias@gmail.com>
2017-10-19 12:20:32 +0200

    english menu

--- functions.php
+++ functions.php
@@ -381,6 +381,7 @@ function vc_before_init_actions() {
     require_once( get_template_directory().'/vc-elements/titre-ancre.php' );
     require_once( get_template_directory().'/vc-elements/dotted-hr.php' );
     require_once( get_template_directory().'/vc-elements/exergue.php' );
+    require_once( get_template_directory().'/vc-elements/titre-ancre.php' );
 
 }
 
