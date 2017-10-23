3f6c386378e1dfb0b49025a265acc76a51105e0b
Tamias <imbert.matthias@gmail.com>
2017-10-23 12:17:39 +0200

    TMS glyphes

--- vc-elements/glyphe.php
+++ vc-elements/glyphe.php
@@ -66,7 +66,7 @@ class vcGlyphe extends WPBakeryShortCode {
         
         //ADD <wbr/> between each letter to avoid chrome not breaking word between slashes.
 	for ($i = 0; $i < strlen($text); $i+=6){
-	    if( substr($text, $i ,$i+4) = "</br>"){
+	    if( substr($text, $i ,$i+4) == "</br>"){
 	    	//DONT PUT <wbr/> inside </br> tag
 	    	$i=$i+4;
 	    }
