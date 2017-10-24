5f658215a23422cfc3c49b616b34067c1ea5a25e
Tamias <imbert.matthias@gmail.com>
2017-10-24 13:24:33 +0200

    TMS equipe shortcode

--- template-recherche-axes-programmes.php
+++ template-recherche-axes-programmes.php
@@ -109,7 +109,34 @@ get_header(); ?>
 					</a>
 				<?php } ?>
 			</div>
-
+			
+			<div class="wpb_column vc_column_container vc_col-sm-12">
+				<div class="vc_column-inner ">
+					<div class="wpb_wrapper">
+						<div class="vc_separator wpb_content_element vc_separator_align_center vc_sep_width_100 vc_sep_pos_align_center vc_separator_no_text vc_sep_color_grey">
+							<span class="vc_sep_holder vc_sep_holder_l"><span class="vc_sep_line"></span></span>
+							<span class="vc_sep_holder vc_sep_holder_r"><span class="vc_sep_line"></span></span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="vc_row wpb_row vc_row-fluid">
+				<div class="wpb_column vc_column_container vc_col-sm-6">
+					<div class="vc_column-inner ">
+						<div class="wpb_wrapper">
+						</div>
+					</div>
+				</div>
+				<div class="wpb_column vc_column_container vc_col-sm-6">
+					<div class="vc_column-inner ">
+						<div class="wpb_wrapper">
+						        <div class="vc-lienpagesuivante-wrap typo_beta" "="">
+								<a class="vc-lienpagesuivante" href="<?php get_permalink(94); //Page évènements et publications ?>">→&nbsp;évènements et publications</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 
 
 		</main><!-- #main -->
