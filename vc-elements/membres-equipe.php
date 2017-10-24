9baa7a1670f15a02e987a8cd107c690240326c55
Tamias <imbert.matthias@gmail.com>
2017-10-24 12:15:07 +0200

    TMS equipe shortcode

--- vc-elements/membres-equipe.php
+++ vc-elements/membres-equipe.php
@@ -59,73 +59,73 @@ function shortcode_etablissements_partenaires( $atts ){
 	$assistants = get_posts($args);
   	
 	$template = '<div class="columns_wrapper typo_epsilon equipe">';
-		$template = '<div class="half_column first_column">';
-			$template = '<ul id="membres_administration" class="membres_equipe">';
+		$template .= '<div class="half_column first_column">';
+			$template .= '<ul id="membres_administration" class="membres_equipe">';
 				foreach($membres_administration as $membre){ 
-					$template = '<li class="membre">';
-						$template = '<span class="nom_complet">'.$membre->post_title.'</span>';
+					$template .= '<li class="membre">';
+						$template .= '<span class="nom_complet">'.$membre->post_title.'</span>';
 						$fonction = get_post_meta($membre->ID, 'wpcf-fonction', true);
 						if($fonction != ''){
-						$template = '</br><span class="fonction">'.$fonction.'</span>';
+						$template .= '</br><span class="fonction">'.$fonction.'</span>';
 						}	
 
-						$template = '</br></br>';
-					$template = '</li>';
+						$template .= '</br></br>';
+					$template .= '</li>';
 				}
-			$template = '</ul>';
-			$template = '</br>';
-			$template = '</br>';
-			$template = '<h3>PROFESSEURS D’ANGLAIS</h3>';
-			$template = '<ul id="professeurs_anglais" class="membres_equipe">';
+			$template .= '</ul>';
+			$template .= '</br>';
+			$template .= '</br>';
+			$template .= '<h3>PROFESSEURS D’ANGLAIS</h3>';
+			$template .= '<ul id="professeurs_anglais" class="membres_equipe">';
 				foreach($professeurs_anglais as $membre){
-					$template = '<li class="membre">';
-						$template = '<span class="nom_complet">'.$membre->post_title.'</span>';
-						$template = '</br></br>';
-					$template = '</li>';
+					$template .= '<li class="membre">';
+						$template .= '<span class="nom_complet">'.$membre->post_title.'</span>';
+						$template .= '</br></br>';
+					$template .= '</li>';
 				}
-			$template = '</ul>';
-		$template = '</div>';
-		$template = '<div class="half_column last_column">';
-			$template = '<h3>PROFESSEURS</h3>';
-			$template = '<ul id="membres_administration" class="membres_equipe">';
+			$template .= '</ul>';
+		$template .= '</div>';
+		$template .= '<div class="half_column last_column">';
+			$template .= '<h3>PROFESSEURS</h3>';
+			$template .= '<ul id="membres_administration" class="membres_equipe">';
 				foreach($professeurs as $membre){
-					$template = '<li class="membre">';
+					$template .= '<li class="membre">';
 						if($membre->post_content != ''){
-							$template = '<a id="'.$membre->ID.'" class="link_professeur nom_complet open_in_popup" href="'.get_permalink($membre->ID).'">'.$membre->post_title.'</a>';
+							$template .= '<a id="'.$membre->ID.'" class="link_professeur nom_complet open_in_popup" href="'.get_permalink($membre->ID).'">'.$membre->post_title.'</a>';
 						} else{
-							$template = '<span class="nom_complet">'.$membre->post_title.'</span>';
+							$template .= '<span class="nom_complet">'.$membre->post_title.'</span>';
 						}
 						$fonction = get_post_meta($membre->ID, 'wpcf-fonction', true);
 						if($fonction != ''){
-							$template = '</br><span class="fonction">'.$fonction.'</span>';
+							$template .= '</br><span class="fonction">'.$fonction.'</span>';
 						}
 
-						$template = '</br></br>';
-					$template = '</li>';
+						$template .= '</br></br>';
+					$template .= '</li>';
 				}
-			$template = '</ul>';
-			$template = '</br>';
-			$template = '</br>';
-			$template = '<h3>ASSISTANTS D\'ENSEIGNEMENT</h3>';
-			$template = '<ul id="assistants" class="membres_equipe">';
+			$template .= '</ul>';
+			$template .= '</br>';
+			$template .= '</br>';
+			$template .= '<h3>ASSISTANTS D\'ENSEIGNEMENT</h3>';
+			$template .= '<ul id="assistants" class="membres_equipe">';
 				foreach($assistants as $membre){
-					$template = '<li class="membre">';
-						$template = '<span class="nom_complet">'.$membre->post_title.'</span>';
+					$template .= '<li class="membre">';
+						$template .= '<span class="nom_complet">'.$membre->post_title.'</span>';
 						$fonction = get_post_meta($membre->ID, 'wpcf-fonction', true);
 						if($fonction != ''){
-							$template = '</br><span class="fonction">'.$fonction.'</span>';
+							$template .= '</br><span class="fonction">'.$fonction.'</span>';
 						}
 
-						$template = '</br></br>';
-					$template = '</li>';
+						$template .= '</br></br>';
+					$template .= '</li>';
 				}
-			$template = '</ul>';
-		$template = '</div>';
-	$template = '</div>';
+			$template .= '</ul>';
+		$template .= '</div>';
+	$template .= '</div>';
 		
 	return $template;
 }
-add_shortcode( 'etablissements_partenaires', 'shortcode_etablissements_partenaires' );
+add_shortcode( 'membres_equipe', 'shortcode_membres_equipe' );
 
 
 ?>
