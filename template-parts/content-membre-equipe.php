3aeb4462258b32598e9151532b5630020bcbddef
Tamias <imbert.matthias@gmail.com>
2017-11-02 08:17:32 +0100

    TMS overlay

--- template-parts/content-membre-equipe.php
+++ template-parts/content-membre-equipe.php
@@ -9,7 +9,7 @@
 ?>
 
 <div id="post_overlay" style="top: 50px; display: block; visibility: visible;">
-	 <div id="post_overlay_close_and_redirect"><a href="<?php echo get_permalink(44); ?>"><img src="http://lab.airlab.fr/esacm/wp-content/themes/esacm/img/close_black.svg" id="post_overlay_close_img"></a></div>
+	 <div id="post_overlay_close_and_redirect"><a href="<?php echo get_permalink(44); //Page Équipe ?>"><img src="http://lab.airlab.fr/esacm/wp-content/themes/esacm/img/close_black.svg" id="post_overlay_close_img"></a></div>
 	 <div id="post_overlay_content">
 	 	<h1 class="actuOpenedTitle typo_alpha"><?php echo get_the_title(); ?></h1>
 		<div class="actuOpenedSubtitle typo_epsilon"><?php echo get_post_meta($post->ID, 'wpcf-fonction', true); ?></div>
@@ -18,3 +18,4 @@
 		</div>
 
 </div><!-- #post-<?php the_ID(); ?> -->
+<a id="false_popup_overlay_under" href="<?php echo get_permalink(44); //Page Équipe ?>"></a>
