b38cbb2487cad2240fbf4c9c7bfcb6c2fa9bbffc
Tamias <imbert.matthias@gmail.com>
2017-11-02 08:26:15 +0100

    TMS overlay

---
+++ template-parts/content-axe.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package hmsphr
+ */
+?>
+
+<div id="post_overlay" style="top: 50px; display: block; visibility: visible;">
+	 <div id="post_overlay_close_and_redirect"><a href="<?php echo get_permalink(93); //Page Axes et programmes ?>"><img src="http://lab.airlab.fr/esacm/wp-content/themes/esacm/img/close_black.svg" id="post_overlay_close_img"></a></div>
+	 <div id="post_overlay_content">
+	 	<h1 class="actuOpenedTitle typo_alpha"><?php echo get_the_title(); ?></h1>
+		<div class="actuOpenedContent typo_epsilon entry-content">
+			<?php the_content(); ?>
+		</div>
+
+</div><!-- #post-<?php the_ID(); ?> -->
+<a id="false_popup_overlay_under" href="<?php echo get_permalink(93); //Page Axes et programmes ?>"></a>
