50a84cce1abba5f1a69a5e0818ac45866e7e8641
Tamias <imbert.matthias@gmail.com>
2017-11-07 14:10:07 +0100

    TMS searchin

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -9,7 +9,7 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('search_result'); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('search_result typo_epsilon'); ?>>
 	<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 	<?php the_excerpt(); ?>
 </article><!-- #post-<?php the_ID(); ?> -->
