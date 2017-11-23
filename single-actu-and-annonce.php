71b7aa8583bf49d48b3845492e4dfb6a9779a7ea
Jeremie <macosphere@Mac-mini-de-Macosphere.local>
2017-11-23 14:22:04 +0100

    Annonces hover

--- single-actu-and-annonce.php
+++ single-actu-and-annonce.php
@@ -40,7 +40,7 @@ if($post_type=='actu'){ ?>
 
 <!-- /////////////////// ANNONCES  //////////////// -->
 
-<div class="post annonce shadowedBox" id="<?php the_ID(); ?>" permalink="<?php echo get_post_permalink(); ?>" keywords="<?php foreach ($keyword_list as $keyword) { echo ($keyword->slug.' '); if($keyword->parent!=0){ $parent=get_term_by('id', $keyword->parent,'post_keyword'); echo $parent->slug.' ';}}?>">
+<div class="post annonce" id="<?php the_ID(); ?>" permalink="<?php echo get_post_permalink(); ?>" keywords="<?php foreach ($keyword_list as $keyword) { echo ($keyword->slug.' '); if($keyword->parent!=0){ $parent=get_term_by('id', $keyword->parent,'post_keyword'); echo $parent->slug.' ';}}?>">
 <div class="annonceText typo_beta"><?php echo types_render_field("annonce_description"); ?></div>
 <a class="annonceLink" href=<?php echo types_render_field("annonce_url", array("output"=>"raw")); ?>
 	<?php if(strpos(types_render_field("annonce_url", array("output"=>"raw")), 'esacm')==false){ echo 'target="_blank"'; }else{ echo 'target="_self"'; } ?>
