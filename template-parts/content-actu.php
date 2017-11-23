62b4f5b5fa8dde0a376c51c4bc1236d24e32a032
Jeremie <macosphere@Mac-mini-de-Macosphere.local>
2017-11-23 15:39:53 +0100

    Title ajax

--- template-parts/content-actu.php
+++ template-parts/content-actu.php
@@ -12,7 +12,7 @@
 <div id="post_overlay" style="top: 25px; display: block; visibility: visible;">
 	 <div id="post_overlay_close_and_redirect"><a href="<?php echo site_url(); ?>#actu"><img src="http://lab.airlab.fr/esacm/wp-content/themes/esacm/img/close_black.svg" id="post_overlay_close_img"></a></div>
 	 <div id="post_overlay_content">
-	 	<h1 class="actuOpenedTitl typo_alpha"><?php echo get_the_title(); ?></h1>
+	 	<h1 class="actuOpenedTitle typo_alpha"><?php echo get_the_title(); ?></h1>
 		<div class="actuOpenedSubtitle  typo_beta"><?php echo types_render_field("subtitle"); ?></div>
 		<div class="actuOpenedContent entry-content typo_beta">
 			<?php the_content(); ?>
