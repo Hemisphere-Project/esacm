e9c8b7ac28a4b336807f32da3a1a8c2c987168d7
Jeremie <macosphere@Mac-mini-de-Macosphere.local>
2017-11-28 17:15:29 +0100

    New exergue

--- vc-elements/exergue.php
+++ vc-elements/exergue.php
@@ -2,32 +2,32 @@
 /*
 Element Description: VC Intro Text
 */
- 
-// Element Class 
+
+// Element Class
 class vcExergue extends WPBakeryShortCode {
-     
+
     // Element Init
     function __construct() {
         add_action( 'init', array( $this, 'vc_exergue_mapping' ) );
         add_shortcode( 'vc_exergue', array( $this, 'vc_exergue_html' ) );
     }
-     
+
     // Element Mapping
     public function vc_exergue_mapping() {
-         
+
         // Stop all if VC is not enabled
         if ( !defined( 'WPB_VC_VERSION' ) ) {
             return;
         }
-         
+
         // Map the block with vc_map()
-        vc_map( 
+        vc_map(
             array(
                 'name' => __('VC Exergue', 'text-domain'),
                 'base' => 'vc_exergue',
-                'description' => __('Exergue', 'text-domain'), 
-                'params' => array(   
-                         
+                'description' => __('Exergue', 'text-domain'),
+                'params' => array(
+
                     array(
                         'type' => 'textfield',
                         'holder' => 'h2',
@@ -39,44 +39,48 @@ class vcExergue extends WPBakeryShortCode {
                         'admin_label' => false,
                         'weight' => 0,
                         'group' => 'Custom Group',
-                    )                    
-                        
+                    )
+
                 ),
             )
-        );                                
-        
+        );
+
     }
-     
-     
+
+
     // Element HTML
     public function vc_exergue_html( $atts ) {
-         
+
         // Params extraction
         extract(
             shortcode_atts(
                 array(
                     'exergue'   => ''
-                ), 
+                ),
                 $atts
             )
         );
-         
+
         // Fill $html var with data
         $html = '
-        <div class="vc-exergue-wrap typo_alpha">
-         
-            <p class="vc-exergue">' . $exergue . '</p>
-             
-        </div>';      
-         
+
+        <div class="vc-exergue-wrap">
+          <div class="spacer shadowedBox"></div>
+            <div class="vc-exergue-text typo_alpha">
+                <p class="vc-exergue">' . $exergue . '</p>
+            </div>
+          <div class="spacer shadowedBox"></div>
+        </div>'
+        ;
+
         return $html;
-         
+
     }
-     
+
 } // End Element Class
- 
- 
+
+
 // Element Class Init
-new vcExergue(); 
+new vcExergue();
 
 ?>
