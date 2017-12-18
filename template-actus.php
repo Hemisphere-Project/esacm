af2571242561f48192baa4a9dfce41e7a86e01e7
Jeremie <JeremieForge@users.noreply.github.com>
2017-12-18 12:05:43 +0100

    actus

---
+++ template-actus.php
@@ -0,0 +1,77 @@
+<?php
+/*
+ * Template Name: Page Actus
+ * @package hmsphr
+ */
+
+ get_header(); ?>
+
+ 	<div id="primary" class="content-area">
+ 		<main id="main" class="site-main">
+
+   <!-- ////////////////////////////////////////////// -->
+   <!-- ////////////////// POST OVERLAY ////////////// -->
+   <!-- ////////////////////////////////////////////// -->
+
+
+    <div id="post_overlay_under">
+      <div id="post_overlay">
+     	 <div id="post_overlay_close"></div>
+     	 <div id="post_overlay_content"></div>
+     </div>
+    </div>
+
+
+   <!-- ////////////////////////////////////////////// -->
+   <!-- /////////////////// ACTU LOOP //////////////// -->
+   <!-- ////////////////////////////////////////////// -->
+
+  	 <section class="actus">
+
+  		 <div class="title typo_alpha">ÉVÈNEMENTS &amp; ACTUALITÉS </div>
+
+
+   <!-- //////////////////// LOOP  ////////////////// -->
+  		<div class="postsTable">
+  			<div class="grid-sizer"></div>
+  			<div class="gutter-sizer"></div>
+
+  		 <?php
+
+       $tags = explode(",", $_GET["tag"]);
+
+       if($tags){
+          $args = array(
+           'post_type' => array('actu', 'annonce'),
+           'posts_per_page'=> -1,
+           'tax_query' => array(
+             array(
+                 'taxonomy' => 'post_keyword',
+                 'field' => 'slug',
+                 'terms' => $tags
+             )
+           )
+          );
+        }else{
+          $args = array(
+           'post_type' => array('actu', 'annonce'),
+           'posts_per_page'=> -1
+          );
+        }
+
+  		  $loop = new WP_Query( $args );
+  		  while ( $loop->have_posts() ) : $loop->the_post();
+
+  				get_template_part( 'single-actu-and-annonce');
+
+   			endwhile; ?>
+  		</div>
+
+  	 </section>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+//get_sidebar();
+get_footer();
