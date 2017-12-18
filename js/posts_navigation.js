0cb0260149d4e73d8cfbf13ca93e4ab034782f66
Jeremie <macosphere@Mac-mini-de-Macosphere.local>
2017-12-18 14:44:17 +0100

    //

--- js/posts_navigation.js
+++ js/posts_navigation.js
@@ -216,7 +216,7 @@ $(function() {
       imagesLoaded: true,
       wrapAround: true,
       setGallerySize: true, // calcule la hauteur de la galerie en fonction de l'image la plus haute
-      adaptiveHeight: true,
+      adaptiveHeight: true, // Changes height of carousel to fit height of selected slide.
       arrowShape: {x0: 10,x1: 60, y1: 50,x2: 65, y2: 45,x3: 20}
     });
 
