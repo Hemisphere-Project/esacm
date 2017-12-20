16bdaca3457c6ae719dd0fa892d961b2353c04e7
Jeremie <JeremieForge@users.noreply.github.com>
2017-12-20 17:58:55 +0100

    template ecole equipe update too

--- template-ecole-equipe.php
+++ template-ecole-equipe.php
@@ -4,59 +4,71 @@
  * @package hmsphr
  */
 
-//GET ADMINISTRATION MEMBERS
-$args =	array(
-	'post_type'=>'membre-equipe',
-	'posts_per_page'=> -1,
-	'tax_query' => array(
-		array(
-			'taxonomy' => 'activite',
-			'terms' => 16
-		)
-	),
-	'order' => 'ASC'
-);
-$membres_administration = get_posts($args);
 
-//GET ENGLISH PROFESSORS
-$args =	array(
-	'post_type'=>'membre-equipe',
-	'posts_per_page'=> -1,
-	'tax_query' => array(
-		array(
-			'taxonomy' => 'activite',
-			'terms' => 17
-		)
-	),
-	'order' => 'ASC'
-);
-$professeurs_anglais = get_posts($args);
-//GET OTHER PROFESSORS
-$args =	array(
-	'post_type'=>'membre-equipe',
-	'posts_per_page'=> -1,
-	'tax_query' => array(
-		array(
-			'taxonomy' => 'activite',
-			'terms' => 18
-		)
-	),
-	'order' => 'ASC'
-);
-$professeurs = get_posts($args);
-//GET Assistants d’enseignement
-$args =	array(
-	'post_type'=>'membre-equipe',
-	'posts_per_page'=> -1,
-	'tax_query' => array(
-		array(
-			'taxonomy' => 'activite',
-			'terms' => 19
-		)
-	),
-	'order' => 'ASC'
-);
-$assistants = get_posts($args);
+   //GET DIRECTION MEMBERS
+ 	$args =	array(
+ 		'post_type'=>'membre-equipe',
+ 		'posts_per_page'=> -1,
+ 		'tax_query' => array(array( 'taxonomy' => 'activite', 'field' => 'slug','terms' => 'direction')),
+ 		'order' => 'ASC'
+ 	);
+ 	$membres_direction = get_posts($args);
+
+ 	//GET BIBLIOTHEQUE MEMBERS
+ 	$args =	array(
+ 		'post_type'=>'membre-equipe',
+ 		'posts_per_page'=> -1,
+ 		'tax_query' => array(array( 'taxonomy' => 'activite', 'field' => 'slug','terms' => 'bibliotheque')),
+ 		'order' => 'ASC'
+ 	);
+ 	$membres_bibliotheque = get_posts($args);
+
+ 	//GET ADMINISTRATION MEMBERS
+ 	$args =	array(
+ 		'post_type'=>'membre-equipe',
+ 		'posts_per_page'=> -1,
+ 		'tax_query' => array(array( 'taxonomy' => 'activite', 'field' => 'slug','terms' => 'administration')),
+ 		'order' => 'ASC'
+ 	);
+ 	$membres_administration = get_posts($args);
+
+ 	//GET BATIMENT MEMBERS
+ 	$args =	array(
+ 		'post_type'=>'membre-equipe',
+ 		'posts_per_page'=> -1,
+ 		'tax_query' => array(array( 'taxonomy' => 'activite', 'field' => 'slug','terms' => 'batiment')),
+ 		'order' => 'ASC'
+ 	);
+ 	$membres_batiment = get_posts($args);
+
+ 	//GET PROFESSEURS MEMBERS
+ 	$args =	array(
+ 		'post_type'=>'membre-equipe',
+ 		'posts_per_page'=> -1,
+ 		'tax_query' => array(array( 'taxonomy' => 'activite', 'field' => 'slug','terms' => 'professeurs')),
+ 		'order' => 'ASC'
+ 	);
+ 	$membres_professeurs = get_posts($args);
+
+ 	//GET ASSISTANTS D'ENSEIGNEMENT MEMBERS
+ 	$args =	array(
+ 		'post_type'=>'membre-equipe',
+ 		'posts_per_page'=> -1,
+ 		'tax_query' => array(array( 'taxonomy' => 'activite', 'field' => 'slug','terms' => 'assistants-denseignement')),
+ 		'order' => 'ASC'
+ 	);
+ 	$membres_assistants = get_posts($args);
+
+ 	//GET TECHNICIENS MEMBERS
+ 	$args =	array(
+ 		'post_type'=>'membre-equipe',
+ 		'posts_per_page'=> -1,
+ 		'tax_query' => array(array( 'taxonomy' => 'activite', 'field' => 'slug','terms' => 'techniciens')),
+ 		'order' => 'ASC'
+ 	);
+ 	$membres_techniciens = get_posts($args);
+
+
 
 wp_reset_query();
 
@@ -88,6 +100,68 @@ get_header(); ?>
 			?>
 			<div class="columns_wrapper typo_epsilon equipe">
 				<div class="half_column first_column">
+
+					<h3>DIRECTION</h3>
+					<ul id="membres_direction" class="membres_equipe">
+						<?php foreach($membres_direction as $membre){ ?>
+							<!-- CONTENT -->
+							<?php if($membre->post_content != ''){ ?>
+								<li class="membre has_related_post">
+									<div id="<?php echo $membre->ID; ?>" class="link_professeur open_in_popup" href="<?php echo get_permalink($membre->ID); ?>">
+										<div class="nom_complet"><?php echo $membre->post_title; ?></div>
+										<?php $fonction = get_post_meta($membre->ID, 'wpcf-fonction', true);
+										if($fonction != ''){?>
+										<div class="fonction"><?php echo $fonction; ?></div>
+										<?php }	?>
+									</div>
+									</br>
+								</li>
+							<!-- NO CONTENT -->
+							<?php } else{ ?>
+								<li class="membre">
+									<span class="nom_complet"><?php echo $membre->post_title; ?></span>
+									<?php $fonction = get_post_meta($membre->ID, 'wpcf-fonction', true);
+									if($fonction != ''){?>
+									</br><span class="fonction"><?php echo $fonction; ?></span>
+									<?php }	?>
+									</br>
+									</br>
+								</li>
+							<?php } ?>
+						<?php } ?>
+					</ul>
+
+					<h3>BIBLIOTHÈQUE</h3>
+					<ul id="membres_bibliotheque" class="membres_equipe">
+						<?php foreach($membres_bibliotheque as $membre){ ?>
+							<!-- CONTENT -->
+							<?php if($membre->post_content != ''){ ?>
+								<li class="membre has_related_post">
+									<div id="<?php echo $membre->ID; ?>" class="link_professeur open_in_popup" href="<?php echo get_permalink($membre->ID); ?>">
+										<div class="nom_complet"><?php echo $membre->post_title; ?></div>
+										<?php $fonction = get_post_meta($membre->ID, 'wpcf-fonction', true);
+										if($fonction != ''){?>
+										<div class="fonction"><?php echo $fonction; ?></div>
+										<?php }	?>
+									</div>
+									</br>
+								</li>
+							<!-- NO CONTENT -->
+							<?php } else{ ?>
+								<li class="membre">
+									<span class="nom_complet"><?php echo $membre->post_title; ?></span>
+									<?php $fonction = get_post_meta($membre->ID, 'wpcf-fonction', true);
+									if($fonction != ''){?>
+									</br><span class="fonction"><?php echo $fonction; ?></span>
+									<?php }	?>
+									</br>
+									</br>
+								</li>
+							<?php } ?>
+						<?php } ?>
+					</ul>
+
+					<h3>ADMINISTRATION</h3>
 					<ul id="membres_administration" class="membres_equipe">
 						<?php foreach($membres_administration as $membre){ ?>
 							<!-- CONTENT -->
@@ -116,22 +190,43 @@ get_header(); ?>
 							<?php } ?>
 						<?php } ?>
 					</ul>
-					</br>
-					</br>
-					<h3>PROFESSEURS D’ANGLAIS</h3>
-					<ul id="professeurs_anglais" class="membres_equipe">
-						<?php foreach($professeurs_anglais as $membre){ ?>
-							<li class="membre">
-								<span class="nom_complet"><?php echo $membre->post_title; ?></span>
-								</br></br>
-							</li>
+
+					<h3>BÂTIMENT</h3>
+					<ul id="membres_batiment" class="membres_equipe">
+						<?php foreach($membres_batiment as $membre){ ?>
+							<!-- CONTENT -->
+							<?php if($membre->post_content != ''){ ?>
+								<li class="membre has_related_post">
+									<div id="<?php echo $membre->ID; ?>" class="link_professeur open_in_popup" href="<?php echo get_permalink($membre->ID); ?>">
+										<div class="nom_complet"><?php echo $membre->post_title; ?></div>
+										<?php $fonction = get_post_meta($membre->ID, 'wpcf-fonction', true);
+										if($fonction != ''){?>
+										<div class="fonction"><?php echo $fonction; ?></div>
+										<?php }	?>
+									</div>
+									</br>
+								</li>
+							<!-- NO CONTENT -->
+							<?php } else{ ?>
+								<li class="membre">
+									<span class="nom_complet"><?php echo $membre->post_title; ?></span>
+									<?php $fonction = get_post_meta($membre->ID, 'wpcf-fonction', true);
+									if($fonction != ''){?>
+									</br><span class="fonction"><?php echo $fonction; ?></span>
+									<?php }	?>
+									</br>
+									</br>
+								</li>
+							<?php } ?>
 						<?php } ?>
 					</ul>
+
 				</div>
 				<div class="half_column last_column">
+
 					<h3>PROFESSEURS</h3>
-					<ul id="membres_administration" class="membres_equipe">
-						<?php foreach($professeurs as $membre){ ?>
+					<ul id="membres_professeurs" class="membres_equipe">
+						<?php foreach($membres_professeurs as $membre){ ?>
 							<!-- CONTENT -->
 							<?php if($membre->post_content != ''){ ?>
 								<li class="membre has_related_post">
@@ -156,14 +251,12 @@ get_header(); ?>
 									</br>
 								</li>
 							<?php } ?>
-							
 						<?php } ?>
 					</ul>
-					</br>
-					</br>
+
 					<h3>ASSISTANTS D'ENSEIGNEMENT</h3>
-					<ul id="assistants" class="membres_equipe">
-						<?php foreach($assistants as $membre){ ?>
+					<ul id="membres_assistants" class="membres_equipe">
+						<?php foreach($membres_assistants as $membre){ ?>
 							<!-- CONTENT -->
 							<?php if($membre->post_content != ''){ ?>
 								<li class="membre has_related_post">
@@ -188,9 +281,39 @@ get_header(); ?>
 									</br>
 								</li>
 							<?php } ?>
-							
 						<?php } ?>
 					</ul>
+
+					<h3>TECHNICIENS</h3>
+					<ul id="membres_techniciens" class="membres_equipe">
+						<?php foreach($membres_techniciens as $membre){ ?>
+							<!-- CONTENT -->
+							<?php if($membre->post_content != ''){ ?>
+								<li class="membre has_related_post">
+									<div id="<?php echo $membre->ID; ?>" class="link_professeur open_in_popup" href="<?php echo get_permalink($membre->ID); ?>">
+										<div class="nom_complet"><?php echo $membre->post_title; ?></div>
+										<?php $fonction = get_post_meta($membre->ID, 'wpcf-fonction', true);
+										if($fonction != ''){?>
+										<div class="fonction"><?php echo $fonction; ?></div>
+										<?php }	?>
+									</div>
+									</br>
+								</li>
+							<!-- NO CONTENT -->
+							<?php } else{ ?>
+								<li class="membre">
+									<span class="nom_complet"><?php echo $membre->post_title; ?></span>
+									<?php $fonction = get_post_meta($membre->ID, 'wpcf-fonction', true);
+									if($fonction != ''){?>
+									</br><span class="fonction"><?php echo $fonction; ?></span>
+									<?php }	?>
+									</br>
+									</br>
+								</li>
+							<?php } ?>
+						<?php } ?>
+					</ul>
+
 				</div>
 			</div>
 		</main><!-- #main -->
